45b1a757f5b6eb508e0c6e99be665fda552ef6ff
Amin Abdillah <andiabdillah255@gmail.com>
2023-07-27 14:53:14 +0700

    update styles on post footer pages

--- resources/views/dashboard/footers/create.blade.php
+++ resources/views/dashboard/footers/create.blade.php
@@ -1,74 +1,74 @@
 @extends('dashboard.layouts.main')
 
 @section('container')
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Create a New Footer</h1>
+<div class="col-lg-8">
+  <h1 class="text-2xl font-bold my-4">Create a New Footer</h1>
+  <div class="divider"></div>
+  <form method="post" action="/dashboard/footers" class="mb-5">
+    @csrf
+    <div class="mb-3">
+      <label for="name" class="text-xl font-bold">CSIRT Name</label>
+      <input type="text" class="form-input @error('name') is-invalid @enderror" id="name" name="name" required autofocus value="{{ old('name') }}">
+      @error('name')
+      <div class="invalid-feedback">
+        {{ $message }}
+      </div>
+      @enderror
     </div>
 
-    <div class="col-lg-8">
-        <form method="post" action="/dashboard/footers" class="mb-5">
-            @csrf
-            <div class="mb-3">
-                <label for="name" class="form-label">CSIRT Name</label>
-                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" required autofocus value="{{ old('name') }}">
-                @error('name')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-
-            <div class="mb-3">
-                <label for="address" class="form-label">Address</label>
-                <input type="text" class="form-control @error('address') is-invalid @enderror" id="address" name="address" required value="{{ old('address') }}">
-                @error('address')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-
-            <div class="mb-3">
-                <label for="maps" class="form-label">Maps</label>
-                <textarea class="form-control @error('maps') is-invalid @enderror" id="maps" name="maps" required rows="5">{{ old('maps') }}</textarea>
-                @error('maps')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-
-            <div class="mb-3">
-                <label for="telephone" class="form-label">Telephone</label>
-                <input type="text" class="form-control @error('telephone') is-invalid @enderror" id="telephone" name="telephone" required value="{{ old('telephone') }}">
-                @error('telephone')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" required value="{{ old('email') }}">
-                @error('email')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-
-            <button type="submit" class="btn btn-primary">Create Footer</button>
-        </form>
+    <div class="mb-3">
+      <label for="address" class="text-xl font-bold">Address</label>
+      <input type="text" class="form-input @error('address') is-invalid @enderror" id="address" name="address" required value="{{ old('address') }}">
+      @error('address')
+      <div class="invalid-feedback">
+        {{ $message }}
+      </div>
+      @enderror
     </div>
 
+    <div class="mb-3">
+      <label for="maps" class="text-xl font-bold">Maps</label>
+      <textarea class="form-textarea @error('maps') is-invalid @enderror" id="maps" name="maps" required rows="5">{{ old('maps') }}</textarea>
+      @error('maps')
+      <div class="invalid-feedback">
+        {{ $message }}
+      </div>
+      @enderror
+    </div>
+
+    <div class="mb-3">
+      <label for="telephone" class="text-xl font-bold">Telephone</label>
+      <input type="text" class="form-input @error('telephone') is-invalid @enderror" id="telephone" name="telephone" required value="{{ old('telephone') }}">
+      @error('telephone')
+      <div class="invalid-feedback">
+        {{ $message }}
+      </div>
+      @enderror
+    </div>
+
+    <div class="mb-3">
+      <label for="email" class="text-xl font-bold">Email</label>
+      <input type="text" class="form-input @error('email') is-invalid @enderror" id="email" name="email" required value="{{ old('email') }}">
+      @error('email')
+      <div class="invalid-feedback">
+        {{ $message }}
+      </div>
+      @enderror
+    </div>
+    <button type="submit" class="btn btn-add">
+      Create Footer
+      <span class="material-symbols-rounded">
+        check_circle
+      </span>
+    </button>
+  </form>
+</div>
 
 
-    <script nonce="{{ csp_nonce() }}">
-        document.addEventListener('trix-file-accept', function(e){
-            e.preventDefault();
-        })
-    </script>
+
+<script nonce="{{ csp_nonce() }}">
+  document.addEventListener('trix-file-accept', function(e) {
+    e.preventDefault();
+  })
+</script>
 @endsection
-
-
--- resources/views/dashboard/footers/index.blade.php
+++ resources/views/dashboard/footers/index.blade.php
@@ -1,9 +1,6 @@
 @extends('dashboard.layouts.main')
 
 @section('container')
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-  <h1 class="h2">Post Footer</h1>
-</div>
 
 @if (session()->has('success'))
 <div class="alert alert-success col-lg-10" role="alert">
@@ -12,9 +9,55 @@
 @endif
 
 <div class="table-responsive col-lg-10 mb-4">
-  <a href="/dashboard/footers/create" class="btn btn-primary mb-3" data-bs-toggle="tooltip" data-bs-placement="top" title="Create">Create a New Footer</a>
-  <div class="overflow-x-auto">
+  <h1 class="text-2xl font-bold my-4">Post Footer</h1>
+  <div class="divider"></div>
+  <a href="/dashboard/footers/create" class="btn btn-add mb-3">Create a New Footer <span class="material-symbols-rounded">add</span></a>
 
+  <div class="p-4 bg-neutral/70 rounded-[20px] overflow-x-auto">
+    <table class="table">
+      <thead class="text-white">
+        <tr>
+          <th class="p-3">No.</th>
+          <th class="p-3 text-left">Name</th>
+          <th class="p-3 text-left">Address</th>
+          <th class="p-3 text-left">Maps</th>
+          <th class="p-3 text-left">Telephone</th>
+          <th class="p-3 text-left">Email</th>
+          <th class="p-3 text-left">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($footers as $footer)
+        <tr>
+          <td class="p-3">{{ $loop->iteration }} .</td>
+          <td class="p-3">{{ $footer->name }}</td>
+          <td class="p-3">{{ $footer->address }}</td>
+          <td class="p-3 max-w-[150px] lg:max-w-[250px] text-ellipsis overflow-hidden">{{ $footer->maps }}</td>
+          <td class="p-3">{{ $footer->telephone }}</td>
+          <td class="p-3">{{ $footer->email }}</td>
+          <td class="p-3 flex gap-2">
+            <a class="icon-edit tooltip" data-tip="Edit" href="/dashboard/footers/{{ $footer->slug}}/edit">
+              <span class="material-symbols-rounded">
+                edit
+              </span>
+            </a>
+            <form class="tooltip" data-tip="Delete" action="/dashboard/footers/{{ $footer->slug }}" method="post">
+              @method('delete')
+              @csrf
+              <button class="icon-delete" onclick="return confirm('Are you sure?')">
+                <span class="material-symbols-rounded">
+                  delete
+                </span>
+              </button>
+            </form>
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+
+  <!-- <div class="overflow-x-auto">
     <table class="table table-striped table-sm">
       <thead>
         <tr>
@@ -48,6 +91,6 @@
         @endforeach
       </tbody>
     </table>
-  </div>
+  </div> -->
 </div>
 @endsection
